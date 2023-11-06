30223326180abd46f85cc51ba24a7d57f14a93ee
alizoort <73581035+alizoort@users.noreply.github.com>
2023-11-06 17:18:57 +0200

    refactoring

--- lib/shared/text_input_component/constants.dart
+++ lib/shared/text_input_component/constants.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
-
-const kTextInputDecoration = InputDecoration();
+const kTextInputFocusColor = Color(0xff3fba81);
+const kTextInputBorderSide = BorderSide(width: 2.0);
+InputDecoration kTextInputDecoration = InputDecoration(
+  floatingLabelStyle: kTextInputFloatingLabelStyle,
+    focusedBorder: kFocusedTextInputOutlineInputBorder,
+  border: kTextInputOutlineInputBorder
+);
+const kTextInputBorderRadius = BorderRadius.all(Radius.circular(8));
+const kTextInputFloatingLabelStyle = TextStyle(color: kTextInputFocusColor);
+const kTextInputOutlineInputBorder =  OutlineInputBorder(  borderSide: kTextInputBorderSide,borderRadius: kTextInputBorderRadius);
+final kFocusedTextInputOutlineInputBorder = kTextInputOutlineInputBorder.copyWith(borderSide: kTextInputBorderSide.copyWith(color: kTextInputFocusColor));
--- lib/shared/text_input_component/emrc_text_input_component.dart
+++ lib/shared/text_input_component/emrc_text_input_component.dart
@@ -6,7 +6,7 @@ import 'package:modee_emrc_app/shared/text_input_component/constants.dart';
 class TextInputComponent extends StatefulWidget {
   final String fieldName;
   final String label;
-  final InputDecoration decoration;
+  final InputDecoration? decoration;
   final List<String? Function(String?)> validators;
   final bool obscureText;
   final void Function(String?)? onChanged;
@@ -15,8 +15,8 @@ class TextInputComponent extends StatefulWidget {
   final AutovalidateMode? autovalidateMode;
   final int maxLines;
   final dynamic Function(String?)? valueTransformer;
-  const TextInputComponent({super.key,required this.fieldName,required this.label,this.decoration = kTextInputDecoration,this.validators= const[],this.obscureText=false,
-  this.readOnly=false,this.autovalidateMode=AutovalidateMode.disabled,this.maxLines =1,this.onChanged,this.initialValue,this.valueTransformer});
+  const TextInputComponent({super.key,required this.fieldName,required this.label,this.validators= const[],this.obscureText=false,
+  this.readOnly=false,this.autovalidateMode=AutovalidateMode.disabled,this.maxLines =1,this.decoration ,this.onChanged,this.initialValue,this.valueTransformer});
 
   @override
   State<TextInputComponent> createState() => _TextInputComponentState();
@@ -26,6 +26,7 @@ class _TextInputComponentState extends State<TextInputComponent> {
   @override
   Widget build(BuildContext context) {
     return  FormBuilderTextField(
+      cursorColor:kTextInputFocusColor,
       valueTransformer: widget.valueTransformer,
       maxLines: widget.maxLines,
       autovalidateMode: widget.autovalidateMode,
@@ -33,7 +34,7 @@ class _TextInputComponentState extends State<TextInputComponent> {
       initialValue: widget.initialValue,
       onChanged: widget.onChanged ?? (String? t){},
       name: widget.fieldName,
-      decoration: widget.decoration.copyWith(labelText: widget.label),
+      decoration: widget.decoration != null ? widget.decoration?.copyWith(labelText: widget.label) as InputDecoration : kTextInputDecoration.copyWith(labelText: widget.label),
       obscureText: widget.obscureText,
       validator: FormBuilderValidators.compose(widget.validators),
     );
